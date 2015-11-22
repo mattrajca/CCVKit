ae0ed7ca799990e4c5452024bced5e89726a3380
Matt Rajca <matt.rajca@me.com>
2015-11-22 17:05:41 -0600

    Initial commit

---
+++ CCVKit/CCVKit.h
@@ -0,0 +1,19 @@
+//
+//  CCVKit.h
+//  CCVKit
+//
+//  Created by Matt on 11/22/15.
+//  Copyright © 2015 Matt Rajca. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CCVKit.
+FOUNDATION_EXPORT double CCVKitVersionNumber;
+
+//! Project version string for CCVKit.
+FOUNDATION_EXPORT const unsigned char CCVKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCVKit/PublicHeader.h>
+
+
